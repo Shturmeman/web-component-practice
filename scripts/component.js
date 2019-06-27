306ac3162da1ebfd7f1291ec030c8a0c28fdaa02
Shturmeman <valerasasin@gmail.com>
2019-06-27 21:57:29 +0300

    asd

---
+++ scripts/component.js
@@ -0,0 +1,24 @@
+class BaseLogic extends HTMLElement {
+    constructor() {
+        super()
+    }
+    static get observedAttributes() {
+        return ["croco"]
+    }
+    attributeChangedCallback(attrName, oldVal, newVal) {
+        this.querySelector("img").src = newVal
+    }
+    connectedCallback() {
+        Promise.all([
+            fetch("../Chanks/chank1.html"),
+            fetch("../style/chank1.css")
+        ])
+            .then(response => Promise.all(response.map(item => item.text())))
+            .then(response => {
+                this.innerHTML = response[0]
+                this.appendChild(document.createElement("style")).textContent = response[1]
+            })
+    }
+}
+
+customElements.define("base-logic", BaseLogic)
